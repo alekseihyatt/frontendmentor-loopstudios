1c8bd71c00b12c93ed5a41ff2db76065029a7c9f
Aleksey Arsentev <useruser.edd@icloud.com>
2024-03-01 18:44:29 +0300

    prod

--- main.js
+++ main.js
@@ -22,3 +22,20 @@ function updateImageSources() {
 
   // Add event listener for changes in screen width
   window.addEventListener('resize', updateImageSources);
+
+
+  const navbarToggle = navbar.querySelector("#navbar-toggle");
+  const navbarMenu = document.querySelector("#navbar-menu");
+  const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
+  let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
+  
+  const toggleNavbarVisibility = () => {
+    isNavbarExpanded = !isNavbarExpanded;
+    navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
+  };
+  
+  navbarToggle.addEventListener("click", toggleNavbarVisibility);
+  
+  navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
+  navbarMenu.addEventListener("click", toggleNavbarVisibility);
+  
