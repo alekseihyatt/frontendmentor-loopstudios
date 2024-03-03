571f4899edf59a59efdc1eeac44efa2d738ed028
Aleksey Arsentev <useruser.edd@icloud.com>
2024-03-03 18:22:51 +0300

    first

--- main.js
+++ main.js
@@ -41,14 +41,3 @@ function updateImageSources() {
 
   
   /////
-
-  var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-64px";
-  }
-  prevScrollpos = currentScrollPos;
-}   
